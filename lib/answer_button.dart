5adddd1fcc47cd421ef36c6e621a700fd570561c
martinv-23 <martin.villa.hdz@hotmail.com>
2025-06-08 23:27:25 -0600

    Segundo commit

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AnswerButton extends StatelessWidget {
   const AnswerButton(this.text, this.onTap, {super.key});
@@ -11,12 +12,21 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-          backgroundColor:const Color.fromARGB(255, 33, 1, 95),
-          foregroundColor: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
-      child: Text(text, textAlign: TextAlign.center,),
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 30),
+        backgroundColor: const Color(0xFF00F6FF),
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        elevation: 10,
+        shadowColor: Colors.cyanAccent,
+      ),
+      child: Text(
+        text,
+        style: GoogleFonts.orbitron(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
