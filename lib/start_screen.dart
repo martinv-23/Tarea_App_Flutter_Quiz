6dda9d150565a3201fc5acf9f14a76d9d6f87d99
martinv-23 <martin.villa.hdz@hotmail.com>
2025-06-08 23:35:18 -0600

    Octavo commit

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_1/button.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -7,30 +8,34 @@ class StartScreen extends StatelessWidget {
   final void Function() startQuiz;
 
   @override
-  Widget build(context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 250,
-          color: const Color.fromARGB(200, 255, 255, 255),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        const Text(
-          'Learn Flutter the fun way!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250,
+            color: const Color.fromARGB(200, 255, 255, 255),
           ),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        OutlinedButtonExample('Start Quiz',startQuiz, const Icon(Icons.arrow_right_alt)),
-      ],
+          const SizedBox(height: 50),
+          Text(
+            'Prepárate para un desafío Flutter',
+            style: GoogleFonts.orbitron(
+              color: Colors.tealAccent,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 50),
+          OutlinedButtonExample(
+            '¡Empezar Quiz!',
+            startQuiz,
+            const Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
     );
   }
 }
